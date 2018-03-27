07d72031881a5ea728cf2a978e71d206784f82c1
kevinlin6543 <kevinlinderdin@gmail.com>
2018-03-27 14:42:35 -0400

    Add files via upload

--- DSA.cpp
+++ DSA.cpp
@@ -3,31 +3,64 @@
 
 using namespace std;
 
-template <typename Obj>
+template <typename T>
 class SampleList
 {
-protected:
-
-private:
-  struct Node
-  {
-    Obj data;
-    Node *prev;
-    Node *next;
-
-    Node(const Obj & d = Obj(), Node *p = NULL, Node *n = NULL)
-      : data(d), prev(p), next(n){}
-  }
+  protected:
+    void insertAtEnd(T newValue);
+    void insertAtStart(T newValue);
+    T removeFromStart();
+  private:
+    class node
+    {
+      T data;
+      node *prev;
+      node *next;
+    }
+    node *head, *tail;
+    String name;
+  public:
   SimpleList()
   {
     prev = NULL;
     tail = NULL;
   }
+  virtual T pop() = 0;
+  virtual void push(T newValue) = 0;
+  String getName()
+    return name};
+};
 
-public:
-  class const_iterator
+template <typename T>
+  void insertAtStart(T newValue)
   {
-
-
+    node *temp = new node;
+    temp->data = newValue;
+    temp->next = head;
+    head = temp;
   }
-}
+  void insertAtEnd(T newValue)
+  {
+    node *temp = new node;
+    temp->data = newValue;
+    temp->next = NULL;
+    if(head == NULL)
+    {
+      head = temp;
+      tail = temp;
+      temp = NULL;
+    }
+    else
+    {
+      tail->next = temp;
+      tail = temp;
+    }
+  }
+  T removeFromStart()
+  {
+    node *temp = new node;
+    temp = head;
+    head = head->next;
+    return temp->data
+    delete temp;
+  }
