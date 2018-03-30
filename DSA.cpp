7b0ea691f9ca77ce36b9197ea7330f5a292e6632
kevinlin6543 <kevinlinderdin@gmail.com>
2018-03-30 12:10:50 -0400

    Add files via upload

--- DSA.cpp
+++ DSA.cpp
@@ -1,6 +1,8 @@
 #include <iterator>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <list>
 #include <fstream>
 
 using namespace std;
@@ -22,13 +24,20 @@ class SimpleList
     Node* tail;
     string name;
   public:
-    SimpleList();
     virtual T pop() = 0;
     virtual void push(T newValue) = 0;
     string getName(){
       return name;}
+    void setName(string newName){
+        name = newName;}
+    void setNull();
 };
-
+template<typename T>
+void SimpleList<T>::setNull()
+{
+  head = NULL;
+  tail = NULL;
+}
 template <typename T>
 void SimpleList<T>::insertAtStart(T newValue)
   {
@@ -62,53 +71,186 @@ T SimpleList<T>::removeFromStart()
     temp = head;
     head = head->next;
     return temp->data;
-    delete temp;
+    //delete temp;
   }
-
 template <typename T>
 class Stack : public SimpleList<T>
 {
   public:
-    Stack()
+    Stack(string s)
     {
-      SimpleList<T>();
+      this->setName(s);
+      this->setNull();
     }
     T pop(){
       this->removeFromStart();}
-    void push(T newValue);
+    void push(T newValue){
+      this->insertAtStart(newValue);}
 };
-
 template <typename T>
 class Queue : public SimpleList<T>
 {
   public:
-    Queue() {
-      head = NULL;
-      tail = NULL;
+    Queue(string s)
+    {
+      this->setName(s);
+      this->setNull();
     }
     T pop(){
       this->removeFromStart();}
     void push(T newValue) {
-      this->insertAtEnd(newValue);
+      this->insertAtStart(newValue);
+      cout<<"works";
     }
 };
 
-
-
 int main() {
-  Queue<int> test;
-  test.push(15);
-  //test.push(1);
-  //cout<<test.pop();
-  cout<<"Hello programming!\n";
-  //ofstream tester;
-  //tester.open("ex.txt");
-  //tester <<test.pop();
-  //tester << "Hello programming!\n";
-  //tester.close();
+  list<SimpleList<int> *> listSLi;
+  list<SimpleList<double> *> listSLd;
+  list<SimpleList<string> *> listSLs;
 
-  //char *token = std::strtok(input, "\n");
-   //while (token != NULL) {
-     //char *command =
+  Queue<int> test("blank");
+  test.push(15);
+  cout<<test.pop()<<endl;
+  cout<<"Hello programming!\n";
+  ofstream tester;
+  tester.open("ex.txt");
+  tester << "Hello programming!\n";
+  string line;
+  ifstream myfile ("example.txt");
+  while ( myfile.good() )
+  {
+    //cout << "step" << endl;
+    getline (myfile,line);
+    string command = line.substr(0,line.find(' '));
+    string type = line.substr(line.find(' ')+1,1);
+    string listName = line.substr(line.find(' ')+1,line.find(' ',line.find(' '))-line.find(' ')+2);
+    string valuePush = line.substr(line.find(' ',line.find(' ')+1)+1,line.find("\n")-line.find(' ',line.find(' ')));
+    string listType = line.substr(line.find(' ',line.find(' ')+1)+1,5);
+    tester << command << "\n";
+    cout << type << endl;
+    tester << listType<< "\n";
+    if(command == "create")
+    {
+      //cout << "created"<<endl;
+      if (listType == "queue")
+      {
+        cout << "created i" <<endl;
+        if (type == "i")
+        {
+          SimpleList<int> *pSLi;
+          pSLi = new Queue<int>(listName);
+          listSLi.push_front(pSLi);
+
+        }
+        else if (type == "s")
+        {
+          SimpleList<string> *pSLs;
+          pSLs = new Queue<string>(listName);
+          listSLs.push_front(pSLs);
+        }
+        else if (type == "d")
+        {
+          SimpleList<double> *pSLd;
+          pSLd = new Queue<double>(listName);
+          listSLd.push_front(pSLd);
+        }
+      }
+      else if (listType == "stack")
+      {
+        if (type == "i")
+        {
+          SimpleList<int> *pSLi;
+          pSLi = new Stack<int>(listName);
+          listSLi.push_front(pSLi);
+        }
+        else if (type == "s")
+        {
+          SimpleList<string> *pSLs;
+          pSLs = new Stack<string>(listName);
+          listSLs.push_front(pSLs);
+        }
+        else if (type == "d")
+        {
+          SimpleList<double> *pSLd;
+          pSLd = new Stack<double>(listName);
+          listSLd.push_front(pSLd);
+        }
+      }
+      else
+        cout<<"shit"<<endl;
+    }
+    else if (command == "push")
+    {
+      //cout << valuePush << endl;
+      //cout << "pushed"<<endl;
+      if (type == "i")
+      {
+        stringstream si(valuePush);
+        int intPush;
+        si >> intPush;
+        for(list<SimpleList<int> *>::iterator it = listSLi.begin(); it != listSLi.end(); ++it)
+        {
+          if ((*it)->getName() == listName)
+            (*it)->push(intPush);
+            else {}
+        }
+      }
+      else if (type == "s")
+      {
+        for(list<SimpleList<string> *>::iterator st = listSLs.begin(); st != listSLs.end(); ++st)
+        {
+          if ((*st)->getName() == listName)
+            (*st)->push(valuePush);
+            else {}
+        }
+      }
+      else if (type == "d")
+      {
+        stringstream sd(valuePush);
+        double dPush;
+        sd >> dPush;
+        for(list<SimpleList<double> *>::iterator dt = listSLd.begin(); dt != listSLd.end(); ++dt)
+        {
+          if ((*dt)->getName() == listName)
+            (*dt)->push(dPush);
+          else {}
+        }
+      }
+    }
+    else if (command == "pop")
+    {
+      //cout << "popped"<<endl;
+      if (type == "i")
+      {
+        for(list<SimpleList<int> *>::iterator it = listSLi.begin(); it != listSLi.end(); ++it)
+        {
+          if ((*it)->getName() == listName)
+            cout<<"Pop:"<<(*it)->pop()<<endl;
+          else {}
+        }
+      }
+      else if (type == "s")
+      {
+        for(list<SimpleList<string> *>::iterator st = listSLs.begin(); st != listSLs.end(); ++st)
+        {
+          if ((*st)->getName() == listName)
+            cout<<"Pop:"<<(*st)->pop()<<endl;
+          else {}
+        }
+      }
+      else if (type == "d")
+      {
+        for(list<SimpleList<double> *>::iterator dt = listSLd.begin(); dt != listSLd.end(); ++dt)
+        {
+          if ((*dt)->getName() == listName)
+            cout<<"Pop:"<<(*dt)->pop()<<endl;
+          else {}
+        }
+      }
+    }
+  }
+  myfile.close();
+  tester.close();
   return 0;
 }
